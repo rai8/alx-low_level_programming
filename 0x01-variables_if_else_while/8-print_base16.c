e2c782b697a76fdb71cafc9448bed063a2a2f018
rai8 <hytonne@outlook.com>
2022-03-11 22:41:00 +0300

    to base

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
-#include <unistd.h>
 /**
- * main - Entry point
- * Description: Print base 16 numbers
- * Return: Always 0 (success)
+ * main - Starting point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char c;
-	char d = '0';
+int nums;
+int nums1;
 
-	while (d <= '9')
-	{
-		putchar(d);
-		d++;
-	}
-	for (c = 'a'; c <= 'f'; c++)
-	{
-		putchar(c);
-	}
+for (nums = '0'; nums <= '9'; nums++)
+{
+putchar(nums);
+}
+for (nums1 = 'a'; nums1 <= 'f'; nums1++)
+{
+putchar(nums1);
+}
+putchar('\n');
+return (0);
+}
